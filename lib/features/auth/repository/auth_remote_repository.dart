5da781ec656a258af6e281ee9936961058a0fc3a
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-03-24 23:55:24 +0300

    feat(auth): add token validation and user data fetching
    - Added `SpService` for token retrieval
    - Implemented `getUserData` method to validate token and fetch user data
    - Improved error handling and response parsing

--- lib/features/auth/repository/auth_remote_repository.dart
+++ lib/features/auth/repository/auth_remote_repository.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
 
 import 'package:frontend/core/constants/constants.dart';
+import 'package:frontend/core/services/sp_service.dart';
 import 'package:frontend/models/user_model.dart';
 import 'package:http/http.dart' as http;
 
 class AuthRemoteRepository {
+  final spServide = SpService();
+
   Future<UserModel> signUp(
       {required String name,
       required String email,
@@ -32,14 +35,14 @@ class AuthRemoteRepository {
 
   Future<UserModel> signIn(
       {required String email, required String password}) async {
-         try {
+    try {
       final res = await http.post(
         Uri.parse('${Constants.backendUri}/auth/signin'),
         headers: {
           'Content-Type': 'application/json',
         },
         body: jsonEncode(
-          { 'email': email, 'password': password},
+          {'email': email, 'password': password},
         ),
       );
 
@@ -51,5 +54,38 @@ class AuthRemoteRepository {
     } catch (e) {
       throw e.toString();
     }
+  }
+
+  Future<UserModel?> getUserData() async {
+    try {
+      final token = await spServide.getToken();
+      if (token == null) {
+        return null;
       }
+      final res = await http.post(
+        Uri.parse('${Constants.backendUri}/auth/tokenIsValid'),
+        headers: {
+          'Content-Type': 'application/json',
+          'x-auth-token': token,
+        },
+      );
+
+      if (res.statusCode != 200 || jsonDecode(res.body) == false) {
+        return null;
+      }
+
+      final userResponse =
+          await http.post(Uri.parse('${Constants.backendUri}/auth'), headers: {
+        'Content-Type': 'application/json',
+        'x-auth-token': token,
+      });
+      if (userResponse.statusCode != 200) {
+        throw jsonDecode(userResponse.body)['msg'];
+      }
+
+      return UserModel.fromJson(userResponse.body);
+    } catch (_) {
+      return null;
+    }
+  }
 }
