63b19a805883daecaf479a3626a11c497fd08f2e
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-03-24 23:52:29 +0300

    feat(auth): add token storage and user data fetching
    - Added `SpService` for token storage
    - Implemented `getUserData` method to fetch user data
    - Stored token after successful sign-in

--- lib/features/auth/cubit/auth_cubit.dart
+++ lib/features/auth/cubit/auth_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/core/services/sp_service.dart';
 import 'package:frontend/features/auth/repository/auth_remote_repository.dart';
 import 'package:frontend/models/user_model.dart';
 
@@ -7,6 +8,22 @@ part 'auth_state.dart';
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthInitial());
   final AuthRemoteRepository _authRemoteRepository = AuthRemoteRepository();
+  final spService = SpService();
+
+  void getUserData() async {
+    try {
+      emit(AuthLoading());
+      final userModel = await _authRemoteRepository.getUserData();
+      if (userModel != null) {
+        emit(AuthSignedIn(userModel));
+      } else {
+        emit(AuthInitial());
+      }
+    } catch (e) {
+      emit(AuthError(e.toString()));
+      throw e.toString();
+    }
+  }
 
   void signUp({
     required String name,
@@ -29,6 +46,10 @@ class AuthCubit extends Cubit<AuthState> {
       emit(AuthLoading());
       final user =
           await _authRemoteRepository.signIn(email: email, password: password);
+      // store token
+      if (user.token.isNotEmpty) {
+        await spService.setToken(user.token);
+      }
       emit(AuthSignedIn(user));
     } catch (e) {
       emit(AuthError(e.toString()));
